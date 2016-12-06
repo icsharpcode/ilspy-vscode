1a91a87e6811ac4a0843523db571180962cc27dd
Jeremy Meng <jeremy.ymeng@gmail.com>
2016-12-06 15:07:56 -0800

    Add launcher

---
+++ src/msildecompiler/options.ts
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// Modified from https://github.com/OmniSharp/omnisharp-vscode/blob/master/src/omnisharp/options.ts
+
+import * as vscode from 'vscode';
+
+export class Options {
+    constructor(
+        public path?: string,
+        public useMono?: boolean,
+        public loggingLevel?: string) { }
+
+    public static Read(): Options {
+
+        const msildecompilerConfig = vscode.workspace.getConfiguration('msildecompiler');
+
+        const path = msildecompilerConfig.get<string>('path');
+
+        const useMono =msildecompilerConfig.get<boolean>('useMono');
+
+        const loggingLevel = msildecompilerConfig.get<string>('loggingLevel');
+
+
+        return new Options(path, useMono, loggingLevel);
+    }
+}

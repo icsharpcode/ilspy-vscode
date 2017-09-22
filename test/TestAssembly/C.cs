89de4a5240499749f0335b117fb81d4aa0e9b8f6
Jeremy Meng <jeremy.ymeng@gmail.com>
2017-09-22 12:02:12 -0700

    Add a project to generate the assembly as input to unit tests

---
+++ test/TestAssembly/C.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TestAssembly
+{
+    /// <summary>
+    /// Some class
+    /// </summary>
+    public class C
+    {
+        /// <summary>
+        /// some private class field
+        /// </summary>
+        private int _ProgId;
+
+        /// <summary>
+        /// Some public class propertywith getter and setter
+        /// </summary>
+        public int ProgId
+        {
+            get
+            {
+                return _ProgId;
+            }
+            set
+            {
+                _ProgId = value;
+            }
+        }
+
+        /// <summary>
+        /// Some class ctor
+        /// </summary>
+        /// <param name="ProgramId"></param>
+        public C(int ProgramId)
+        {
+            ProgId = ProgramId;
+        }
+    }
+}
---
+++ test/TestAssembly/S.cs
@@ -0,0 +1,15 @@
+﻿namespace TestAssembly
+{
+    using System;
+
+    /// <summary>
+    /// Some struct
+    /// </summary>
+    public struct S
+    {
+        /// <summary>
+        ///  some public struct property with getter and setter.
+        /// </summary>
+        public int Prop { get; set; }
+    }
+}

76463ffa2ef4f7a3016bee117a156cbdcca153b3
Jeremy Meng <jeremy.ymeng@gmail.com>
2017-11-30 21:07:29 -0800

    Move service code under backend folder

---
+++ backend/src/ILSpy.Host/MiddleWare/Endpoints/ListNamespacesResponse.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace ILSpy.Host
+{
+    internal class ListNamespacesResponse
+    {
+        public IEnumerable<string> Namespaces { get; set; }
+    }
+}
--- src/ILSpy.Host/MiddleWare/Endpoints/ListNamespacesRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) .NET Foundation and Contributors. All rights reserved.
-// Licensed under the MIT license. See the LICENSE file in the project root for more information.
-
-namespace ILSpy.Host
-{
-    internal class ListNamespacesRequest : RequestBase
-    {
-    }
-}

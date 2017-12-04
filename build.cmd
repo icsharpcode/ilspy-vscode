948e3e3930f4a0a1c2cbd3147043f3feca1cba9d
Jeremy Meng <jeremy.ymeng@gmail.com>
2017-12-03 23:30:12 -0800

    Build vsix on appveyor

---
+++ build.cmd
@@ -0,0 +1,27 @@
+set build_version=local
+
+if NOT '%1'=='' set build_version=%1
+
+git submodule init
+git submodule update
+
+pushd backend
+dotnet msbuild
+popd
+
+pushd backend\test\ILSpy.Host.Tests
+dotnet test
+popd
+
+pushd backend\src\ILSpy.Host
+rmdir ..\..\..\vscode-extension\bin\ilspy-host /s /q
+dotnet publish -f net461 -c release -r win7-x64 -o  ..\..\..\vscode-extension\bin\ilspy-host
+popd
+
+pushd vscode-extension
+call npm install
+call npm run compile
+call npm test
+
+call npm i vsce -g
+call vsce package -o ilspy-vscode-%build_version%.vsix

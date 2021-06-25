496ce30d80b62159396805f1305c3b7a066fed49
Andreas Weizel <rpinski@awzhome.de>
2021-06-25 22:59:13 +0200

    Show timestamps in extension log

--- vscode-extension/src/OutputWindowLogger.ts
+++ vscode-extension/src/OutputWindowLogger.ts
@@ -1,13 +1,23 @@
-import * as vscode from "vscode";
+import { OutputChannel, window } from "vscode";
 
 export default class OutputWindowLogger {
-  private channel: vscode.OutputChannel;
+  private channel: OutputChannel;
+  private dateTimeFormat: Intl.DateTimeFormat;
 
   constructor() {
-    this.channel = vscode.window.createOutputChannel("ILSpy Extension");
+    this.channel = window.createOutputChannel("ILSpy Extension");
+    this.dateTimeFormat = new Intl.DateTimeFormat("en", {
+      hour: "2-digit",
+      hour12: false,
+      minute: "2-digit",
+      second: "2-digit",
+      fractionalSecondDigits: 3,
+    } as any);
   }
 
   public writeLine(line: string) {
-    this.channel.appendLine(line);
+    this.channel.appendLine(
+      `[${this.dateTimeFormat.format(Date.now())}] ${line}`
+    );
   }
 }

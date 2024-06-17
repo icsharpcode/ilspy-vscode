298a3432b4ceb6bb48756fe2d6441a3e83a3d8c2
Andreas Weizel <rpinski@awzhome.de>
2024-06-18 00:55:03 +0200

    Rework Webpack config and build tasks
    This fixes debugging experience of the extension:
    - Variable names not minified anymore when debugging
    - Build latest code before starting debugging with F5

--- vscode-extension/webpack.config.js
+++ vscode-extension/webpack.config.js
@@ -1,51 +1,48 @@
 //@ts-check
 
-'use strict';
+"use strict";
 
-const path = require('path');
-const webpack = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
 
-/**@type {import('webpack').Configuration}*/
-const config = {
-  target: 'node',
-  entry: './src/extension.ts',
+//@ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+/** @type WebpackConfig */
+const extensionConfig = {
+  target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+
+  entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-    devtoolModuleFilenameTemplate: '../[resource-path]'
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   externals: {
-    vscode: 'commonjs vscode'
+    vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
-    extensions: ['.ts', '.js'],
-    fallback: {
-      // Webpack 5 no longer polyfills Node.js core modules automatically.
-      // see https://webpack.js.org/configuration/resolve/#resolvefallback
-      // for the list of Node.js core module polyfills.
-	  "fs": false
-    }
-  },
-  optimization: {
-	minimize: true,
-	minimizer: [new TerserPlugin({
-    	extractComments: false
-    })]
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
+        exclude: /node_modules/,
         use: [
           {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
-  }
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
+  },
 };
-module.exports = config;
+module.exports = [extensionConfig];

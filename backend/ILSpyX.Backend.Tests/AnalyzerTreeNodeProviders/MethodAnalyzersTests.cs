38b62e15e2f9460f311d3c618c66363d6cddee73
Andreas Weizel <rpinski@awzhome.de>
2024-11-14 23:31:48 +0100

    Fix decompilation of analyzer results outside of analyzed assembly

--- backend/ILSpyX.Backend.Tests/AnalyzerTreeNodeProviders/MethodAnalyzersTests.cs
+++ backend/ILSpyX.Backend.Tests/AnalyzerTreeNodeProviders/MethodAnalyzersTests.cs
@@ -37,9 +37,48 @@ public class MethodAnalyzersTests
             node => {
                 Assert.Equal("StructMethod() : string", node.DisplayName);
                 Assert.Equal("TestAssembly.SomeStruct", node.Description);
+                Assert.Equal(TestHelper.AssemblyPath, node.Metadata?.AssemblyPath);
                 Assert.Equal(NodeType.Method, node.Metadata?.Type);
                 Assert.Equal(callerStructTypeNode.Metadata?.SymbolToken, node.Metadata?.ParentSymbolToken);
                 Assert.Equal(SymbolModifiers.Public, node.SymbolModifiers);
+                Assert.False(node.MayHaveChildren);
+            });
+    }
+
+    [Fact]
+    public async Task UsesDotNetFrameworkSymbol()
+    {
+        var application = await TestHelper.CreateTestApplication();
+        var types = await application.TreeNodeProviders.Namespace.GetChildrenAsync(
+            new NodeMetadata(TestHelper.AssemblyPath, NodeType.Namespace, "TestAssembly", 0, 0));
+        var typeNode = types.First(node => node.Metadata?.Name == "SomeClass");
+        var members = await application.TreeNodeProviders.Type.GetChildrenAsync(typeNode.Metadata);
+        var methodNode = members.First(node => node.Metadata?.Name?.StartsWith("CallsFrameworkMethod") ?? false);
+        var analyzerNodes = await application.TreeNodeProviders.AnalyzersRoot.GetChildrenAsync(methodNode.Metadata);
+        Assert.Collection(analyzerNodes,
+            node => {
+                Assert.Equal("Uses", node.DisplayName);
+                Assert.Equal("Uses", node.Description);
+                Assert.Equal(TestHelper.AssemblyPath, node.Metadata?.AssemblyPath);
+                Assert.Equal(NodeType.Analyzer, node.Metadata?.Type);
+                Assert.Equal("MethodUsesAnalyzer", node.Metadata?.SubType);
+                Assert.Equal(methodNode.Metadata?.SymbolToken, node.Metadata?.SymbolToken);
+                Assert.Equal(methodNode.Metadata?.ParentSymbolToken, node.Metadata?.ParentSymbolToken);
+                Assert.True(node.MayHaveChildren);
+            },
+            node => { },
+            node => { });
+
+        var methodUsesNodes =
+            await application.TreeNodeProviders.Analyzer.GetChildrenAsync(
+                analyzerNodes.First(analyzer => analyzer.Metadata.SubType == "MethodUsesAnalyzer")?.Metadata);
+        Assert.Collection(methodUsesNodes,
+            node => {
+                Assert.Equal("Join(string?, string?[]) : string", node.DisplayName);
+                Assert.Equal("System.String", node.Description);
+                Assert.Contains("System.Private.CoreLib.dll", node.Metadata?.AssemblyPath);
+                Assert.Equal(NodeType.Method, node.Metadata?.Type);
+                Assert.Equal(SymbolModifiers.Public | SymbolModifiers.Static, node.SymbolModifiers);
                 Assert.False(node.MayHaveChildren);
             });
     }

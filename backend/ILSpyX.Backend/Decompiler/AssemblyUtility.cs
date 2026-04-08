da51fd093b12544357b100fd6983af50591a2443
Andreas Weizel <rpinski@awzhome.de>
2026-04-09 00:40:53 +0200

    Fix empty assembly list if one of added assemblies doesn't exist anymore

--- backend/ILSpyX.Backend/Decompiler/AssemblyUtility.cs
+++ backend/ILSpyX.Backend/Decompiler/AssemblyUtility.cs
@@ -1,5 +1,6 @@
 using ICSharpCode.ILSpyX;
 using ILSpyX.Backend.Model;
+using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,39 +11,47 @@ public static class AssemblyUtility
 {
     public static async Task<AssemblyData?> CreateAssemblyDataAsync(LoadedAssembly loadedAssembly)
     {
-        var loadResult = await loadedAssembly.GetLoadResultAsync();
-        if (loadResult.MetadataFile is not null)
+        try
         {
-            var version = loadResult.MetadataFile.Metadata.GetAssemblyDefinition().Version;
-            string targetFrameworkId = await loadedAssembly.GetTargetFrameworkIdAsync();
-            return new AssemblyData
+            var loadResult = await loadedAssembly.GetLoadResultAsync();
+            if (loadResult.MetadataFile is not null)
             {
-                Name = loadedAssembly.ShortName,
-                FilePath = loadedAssembly.FileName,
-                ParentBundleFilePath = loadedAssembly.ParentBundle?.FileName,
-                IsAutoLoaded = loadedAssembly.IsAutoLoaded,
-                Version = version.ToString(),
-                TargetFramework = !string.IsNullOrEmpty(targetFrameworkId)
-                    ? targetFrameworkId.Replace("Version=", " ")
-                    : null,
-                PackageType = PackageType.None
-            };
+                var version = loadResult.MetadataFile.Metadata.GetAssemblyDefinition().Version;
+                string targetFrameworkId = await loadedAssembly.GetTargetFrameworkIdAsync();
+                return new AssemblyData
+                {
+                    Name = loadedAssembly.ShortName,
+                    FilePath = loadedAssembly.FileName,
+                    ParentBundleFilePath = loadedAssembly.ParentBundle?.FileName,
+                    IsAutoLoaded = loadedAssembly.IsAutoLoaded,
+                    Version = version.ToString(),
+                    TargetFramework = !string.IsNullOrEmpty(targetFrameworkId)
+                        ? targetFrameworkId.Replace("Version=", " ")
+                        : null,
+                    PackageType = PackageType.None
+                };
+            }
+
+            if (loadResult.Package is not null)
+            {
+                return new AssemblyData
+                {
+                    Name = loadedAssembly.ShortName,
+                    FilePath = loadedAssembly.FileName,
+                    IsAutoLoaded = loadedAssembly.IsAutoLoaded,
+                    PackageType = loadResult.Package.Kind switch
+                    {
+                        LoadedPackage.PackageKind.Zip => PackageType.NuGet,
+                        _ => PackageType.Other
+                    }
+                };
+            }
+        }
+        catch (Exception)
+        {
+            // ignored
         }
 
-        if (loadResult.Package is not null)
-        {
-            return new AssemblyData
-            {
-                Name = loadedAssembly.ShortName,
-                FilePath = loadedAssembly.FileName,
-                IsAutoLoaded = loadedAssembly.IsAutoLoaded,
-                PackageType = loadResult.Package.Kind switch
-                {
-                    LoadedPackage.PackageKind.Zip => PackageType.NuGet,
-                    _ => PackageType.Other
-                }
-            };
-        }
 
         return null;
     }

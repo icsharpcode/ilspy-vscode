f3691b7d766c5e34fc82f1adb597fb6ad6d521b6
Andreas Weizel <rpinski@awzhome.de>
2026-04-20 17:10:57 +0200

    Avoid pseudo-errors in C# output
    We used VS Code's C# languageId before, which triggered several C#
    features we didn't need, sometimes showing red pseudo-errors in editor.
    We avoid that now by defining an own ILSpy languageId, which re-uses VS
    Code's built-in grammar for proper syntax highlighting, but nothing else
    C#-related.

--- vscode-extension/src/decompiler/languageInfos.ts
+++ vscode-extension/src/decompiler/languageInfos.ts
@@ -1,5 +1,8 @@
 import { LanguageName } from "../protocol/LanguageName";
 
+const ILSPY_CODE_CS_LANG = "ilspy-code-cs";
+const ILSPY_CODE_IL_LANG = "ilspy-code-il";
+
 export interface LanguageInfo {
   name: LanguageName;
   displayName: string;
@@ -7,91 +10,95 @@ export interface LanguageInfo {
 }
 
 export const languageInfos = createLanguageMap([
-  { name: LanguageName.IL, displayName: "IL", vsLanguageMode: "il" },
+  {
+    name: LanguageName.IL,
+    displayName: "IL",
+    vsLanguageMode: ILSPY_CODE_IL_LANG,
+  },
   {
     name: LanguageName.CSharp_1,
     displayName: "C# 1.0 / VS .NET",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_2,
     displayName: "C# 2.0 / VS 2005",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_3,
     displayName: "C# 3.0 / VS 2008",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_4,
     displayName: "C# 4.0 / VS 2010",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_5,
     displayName: "C# 5.0 / VS 2012",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_6,
     displayName: "C# 6.0 / VS 2015",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_7,
     displayName: "C# 7.0 / VS 2017",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_7_1,
     displayName: "C# 7.1 / VS 2017.3",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_7_2,
     displayName: "C# 7.2 / VS 2017.4",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_7_3,
     displayName: "C# 7.3 / VS 2017.7",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_8,
     displayName: "C# 8.0 / VS 2019",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_9,
     displayName: "C# 9.0 / VS 2019.8",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_10,
     displayName: "C# 10.0 / VS 2022",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_11,
     displayName: "C# 11.0 / VS 2022.4",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_12,
     displayName: "C# 12.0 / VS 2022.8",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_13,
     displayName: "C# 13.0 / VS 2022.12",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
   {
     name: LanguageName.CSharp_14,
     displayName: "C# 14.0 / VS 2026",
-    vsLanguageMode: "csharp",
+    vsLanguageMode: ILSPY_CODE_CS_LANG,
   },
 ]);
 

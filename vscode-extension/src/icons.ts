d96627c47d2bbc965bf73dbfd500aa560aad08cb
Autumn <autumn@puffin.place>
2026-06-06 11:33:18 +1000

    Combine ProductIconMapping and ProductColorMapping

--- vscode-extension/src/icons.ts
+++ vscode-extension/src/icons.ts
@@ -3,70 +3,97 @@
  *  Licensed under the MIT License. See LICENSE.TXT in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import { ThemeIcon, Uri } from "vscode";
+import { ThemeColor, ThemeIcon, Uri } from "vscode";
 import { NodeType } from "./extension-types";
 import * as path from "path";
 
-const UNKNOWN_ICON = { id: "question" };
+const UNKNOWN_ICON = { id: "question", colorKey: "foreground" };
 
 const ProductIconMapping: {
-  [key in NodeType]?: { id: string } | { customIcon: string };
+  [key in NodeType]?: { id: string; colorKey: string } | { customIcon: string };
 } = {
-  [NodeType.Assembly]: { id: "library" },
-  [NodeType.Namespace]: { id: "symbol-namespace" },
-  [NodeType.Event]: { id: "symbol-event" },
-  [NodeType.Field]: { id: "symbol-field" },
-  [NodeType.Method]: { id: "symbol-method" },
-  [NodeType.Enum]: { id: "symbol-enum" },
-  [NodeType.Class]: { id: "symbol-class" },
-  [NodeType.Interface]: { id: "symbol-interface" },
-  [NodeType.Struct]: { id: "symbol-struct" },
-  [NodeType.Delegate]: { id: "symbol-class" },
-  [NodeType.Const]: { id: "symbol-constant" },
-  [NodeType.Property]: { id: "symbol-property" },
-  [NodeType.ReferencesRoot]: { id: "folder-library" },
-  [NodeType.AssemblyReference]: { id: "library" },
+  [NodeType.Assembly]: {
+    id: "library",
+    colorKey: "symbolIcon.packageForeground",
+  },
+  [NodeType.Namespace]: {
+    id: "symbol-namespace",
+    colorKey: "symbolIcon.namespaceForeground",
+  },
+  [NodeType.Event]: {
+    id: "symbol-event",
+    colorKey: "symbolIcon.eventForeground",
+  },
+  [NodeType.Field]: {
+    id: "symbol-field",
+    colorKey: "symbolIcon.fieldForeground",
+  },
+  [NodeType.Method]: {
+    id: "symbol-method",
+    colorKey: "symbolIcon.methodForeground",
+  },
+  [NodeType.Enum]: {
+    id: "symbol-enum",
+    colorKey: "symbolIcon.enumeratorForeground",
+  },
+  [NodeType.Class]: {
+    id: "symbol-class",
+    colorKey: "symbolIcon.classForeground",
+  },
+  [NodeType.Interface]: {
+    id: "symbol-interface",
+    colorKey: "symbolIcon.interfaceForeground",
+  },
+  [NodeType.Struct]: {
+    id: "symbol-struct",
+    colorKey: "symbolIcon.structForeground",
+  },
+  [NodeType.Delegate]: {
+    id: "symbol-class",
+    colorKey: "symbolIcon.classForeground",
+  },
+  [NodeType.Const]: {
+    id: "symbol-constant",
+    colorKey: "symbolIcon.constantForeground",
+  },
+  [NodeType.Property]: {
+    id: "symbol-property",
+    colorKey: "symbolIcon.propertyForeground",
+  },
+  [NodeType.ReferencesRoot]: {
+    id: "folder-library",
+    colorKey: "symbolIcon.folderForeground",
+  },
+  [NodeType.AssemblyReference]: {
+    id: "library",
+    colorKey: "symbolIcon.referenceForeground",
+  },
   [NodeType.Unknown]: UNKNOWN_ICON,
-  [NodeType.Analyzer]: { id: "question" },
-  [NodeType.BaseTypes]: { id: "arrow-up" },
-  [NodeType.DerivedTypes]: { id: "arrow-down" },
+  [NodeType.Analyzer]: {
+    id: "question",
+    colorKey: "symbolIcon.methodForeground",
+  },
+  [NodeType.BaseTypes]: {
+    id: "arrow-up",
+    colorKey: "symbolIcon.interfaceForeground",
+  },
+  [NodeType.DerivedTypes]: {
+    id: "arrow-down",
+    colorKey: "symbolIcon.interfaceForeground",
+  },
   [NodeType.NuGetPackage]: { customIcon: "nuget" },
-  [NodeType.PackageFolder]: { id: "folder" },
-  [NodeType.Resource]: { id: "file" },
-};
-const ProductColorMapping: {
-  [key in NodeType]?: { id: string };
-} = {
-  [NodeType.Assembly]: { id: "symbolIcon.packageForeground" },
-  [NodeType.Namespace]: { id: "symbolIcon.namespaceForeground" },
-  [NodeType.Event]: { id: "symbolIcon.eventForeground" },
-  [NodeType.Field]: { id: "symbolIcon.fieldForeground" },
-  [NodeType.Method]: { id: "symbolIcon.methodForeground" },
-  [NodeType.Enum]: { id: "symbolIcon.enumeratorForeground" },
-  [NodeType.Class]: { id: "symbolIcon.classForeground" },
-  [NodeType.Interface]: { id: "symbolIcon.interfaceForeground" },
-  [NodeType.Struct]: { id: "symbolIcon.structForeground" },
-  [NodeType.Delegate]: { id: "symbolIcon.classForeground" },
-  [NodeType.Const]: { id: "symbolIcon.constantForeground" },
-  [NodeType.Property]: { id: "symbolIcon.propertyForeground" },
-  [NodeType.ReferencesRoot]: { id: "symbolIcon.folderForeground" },
-  [NodeType.AssemblyReference]: { id: "symbolIcon.referenceForeground" },
-  [NodeType.Unknown]: { id: "foreground" },
-  [NodeType.Analyzer]: { id: "symbolIcon.methodForeground" },
-  [NodeType.BaseTypes]: { id: "symbolIcon.interfaceForeground" },
-  [NodeType.DerivedTypes]: { id: "symbolIcon.interfaceForeground" },
-  [NodeType.NuGetPackage]: { id: "symbolIcon.referenceForeground" },
-  [NodeType.PackageFolder]: { id: "symbolIcon.folderForeground" },
-  [NodeType.Resource]: { id: "symbolIcon.fileForeground" },
+  [NodeType.PackageFolder]: {
+    id: "folder",
+    colorKey: "symbolIcon.folderForeground",
+  },
+  [NodeType.Resource]: { id: "file", colorKey: "symbolIcon.fileForeground" },
 };
 
 export function getNodeIcon(nodeType: NodeType | undefined) {
   const iconMapping =
     ProductIconMapping[nodeType ?? NodeType.Unknown] ?? UNKNOWN_ICON;
-  const colorMapping =
-    ProductColorMapping[nodeType ?? NodeType.Unknown] ?? undefined;
   if ("id" in iconMapping) {
-    return new ThemeIcon(iconMapping.id, colorMapping);
+    return new ThemeIcon(iconMapping.id, new ThemeColor(iconMapping.colorKey));
   } else {
     const basePath = path.join(
       __dirname,
